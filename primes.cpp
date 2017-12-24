ee0f0c05b6ea9cb296854f9b3bd58f3dac7a2c22
yashraf74 <34038122+yashraf74@users.noreply.github.com>
2017-12-24 19:22:51 +0200

    Update primes.cpp

--- primes.cpp
+++ primes.cpp
@@ -16,16 +16,20 @@ bool primes::isprime(int n)
 void primes::primechecker()
 {
 	int p;
-	vector<int>notprimes;
+	vector<int>notprimes; //values foe p that aren't primes
+	vector<int>valN; // value for n where p isn't a prime
 	for (int n = 0; n < 1000; n++)
 	{
-		p = n ^ 2 + n + 41;
+		p = pow(n,2) + n + 41;
 		if (isprime(p) != true)
+		{
 			notprimes.push_back(p);
+			valN.push_back(n);
+		}
 	}
 	sort(notprimes.begin(), notprimes.end());
 	notprimes.erase(unique(notprimes.begin(), notprimes.end()), notprimes.end());
 
 	for (int i = 0; i != notprimes.size(); i++)
-		cout << notprimes[i] << "  is not a prime. " << endl;
+		cout << notprimes[i] << "  is not a prime.\t" << "(at n=" << valN[i] << ")" << endl;
 }
