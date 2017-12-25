9fb1a994db46cfe4b849240b260dd6a7cdf2501a
yashraf74 <34038122+yashraf74@users.noreply.github.com>
2017-12-25 02:04:22 +0200

    Update diophantineEq.h

--- diophantineEq.h
+++ diophantineEq.h
@@ -9,6 +9,16 @@ class diophantineEq
 	double a = 1, b = 1, c = 1, d = 1, e, f, valA, valB, valC, valD;
 	double *arrA, *arrB, *arrC, *arrD;
 	int i = 0, j = 0, k = 0, l = 0;
+	
+	/*
+	The values for the variables should be the following:
+	a = 95800
+	b = 217519
+	c = 414560
+	d = 422481
+	Plug it in as initializers for the variables to check that the program is working fine.
+	*/
+	
 public:
 	diophantineEq();
 	void calcDiophEq();
